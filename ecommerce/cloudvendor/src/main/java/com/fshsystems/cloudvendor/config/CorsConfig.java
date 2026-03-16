1f84905f3048414026c767562ef41bd1a5ae2365
MeshackT <meshacktempleton1@gmail.com>
2026-03-16 17:26:04 +0200

    Fixed and removed the "*" from cors

--- ecommerce/cloudvendor/src/main/java/com/fshsystems/cloudvendor/config/CorsConfig.java
+++ ecommerce/cloudvendor/src/main/java/com/fshsystems/cloudvendor/config/CorsConfig.java
@@ -2,25 +2,27 @@ package com.fshsystems.cloudvendor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5500",
-                                "http://127.0.0.1:5500", "*") // frontend origin
-                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedOrigins(
+                                "http://localhost:5500",
+                                "http://127.0.0.1:5500"
+                        )
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
-
-
 }
