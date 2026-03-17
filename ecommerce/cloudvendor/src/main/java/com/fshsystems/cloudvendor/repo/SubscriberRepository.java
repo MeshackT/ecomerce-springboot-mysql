c8112afa3c57cd4a2a4d83aab8056c3004918e44
MeshackT <meshacktempleton1@gmail.com>
2026-03-17 17:42:52 +0200

    Added the subscription form, Fixed the login redirectory, Added the favorites,subscription API, Added the banners, Added Single page view of products

---
+++ ecommerce/cloudvendor/src/main/java/com/fshsystems/cloudvendor/repo/SubscriberRepository.java
@@ -0,0 +1,4 @@
+package com.fshsystems.cloudvendor.repo;
+
+interface SubscriberRepository extends org.springframework.data.jpa.repository.JpaRepository<com.fshsystems.cloudvendor.entity.Subscriber, java.lang.Long> {
+}
